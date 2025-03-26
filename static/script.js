4a974e2ceca99a0d5e73aabb4bf355236ebb55e9
parishijainn <146143376+parishijainn@users.noreply.github.com>
2025-03-25 23:19:52 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,52 +1,3 @@
-// var x, y, z;
-// x = 5;
-// y = 7;
-// z = x+y;
-// console.log(z);
-
-// var A = "Hello ";
-// var B = "world!";
-// var C = A+B;
-// console.log(C);
-
-// function sumNPrint(x1, x2){
-//     let x3 = x1 + x2;
-//     console.log(x3);
-// }
-
-// sumNPrint(x, y);
-// sumNPrint(A, B);
-
-// if (C.length > z){
-//     console.log(C);
-// } else if (C.length < z){
-//     console.log(z);
-// } else{
-//     console.log("good job!");
-// }
-
-// var L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
-// var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
-
-// function findTheBanana(array){
-//     for (let i = 0; i < array.length; i++){
-//         if (arr[i] == "Banana"){
-//             alert("found the Banana in" + i);
-//         }
-//     }
-// }
-
-// function findTheBanana(array, arrayName) {
-//     array.forEach(function(currentValue, index, arr) {
-//         if (currentValue == "Banana") {
-//         alert("We found a Banana in the " + arrayName + " array");
-//         }
-//     });
-// }
-
-// findTheBanana(L1, "first");
-// findTheBanana(L2, "second");
-
 function greetingFunc(){
     var d = new Date()
     var h = d.getHours()
@@ -182,3 +133,34 @@ function getAdvice() {
             console.error("Error:", error);
         });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerIcon = document.getElementById("hamburger-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburgerIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+    });
+});
+
+window.onload = function() {
+    var currentLocation = window.location.pathname; 
+    var links = document.querySelectorAll('.nav-links a'); 
+
+    links.forEach(function(link) {
+        if (link.href.includes(currentLocation)) {
+            link.classList.add('active'); 
+        }
+    });
+};
